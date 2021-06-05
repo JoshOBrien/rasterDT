d5437bb3ac5bd18560aa8e73e19afbfe5eb62d47
JoshOBrien <joshmobrien@gmail.com>
2021-06-05 00:20:43 -0700

    Fix up comment

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
-## Declare these symbols, used in calls to '[.data.table()' , to avoid
-## 'R CMD check' complaints
+## Declare these symbols, used in calls to `[.data.table()`, to avoid
+## 'R CMD check' complaints.
 utils::globalVariables(c(".", "..i", "..j", "Freq", "ID"))
